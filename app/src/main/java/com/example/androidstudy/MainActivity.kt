26efc17164cb46432c5cb29e0310a8a48879205d
Santoragi32 <woojj1254577@naver.com>
2025-03-21 13:27:55 +0900

    [#4]Feat: 액티비티사용 -> 프래그먼트 사용으로 변경, 바텀네비게이션 추가

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -1,41 +1,55 @@
 package com.example.androidstudy
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.androidstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        // ImageButton ID와 연결된 Activity를 Map으로 매핑
-        val buttonMap = mapOf(
-            R.id.happy_stamp to HappyActivity::class.java,
-            R.id.exite_stamp to ExiteActivity::class.java,
-            R.id.normal_stamp to NormalActivity::class.java,
-            R.id.anxious_stamp to AnxiousActivity::class.java,
-            R.id.angry_stamp to AngryActivity::class.java
-        )
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // 반복문을 사용해 리스너 설정
-        buttonMap.forEach { (buttonId, activityClass) ->
-            findViewById<ImageButton>(buttonId).setOnClickListener {
-                startActivity(Intent(this, activityClass))
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        // BottomNavigationView 설정
+        binding.bottomNavi.setupWithNavController(navController)
+
+        // BottomNavigationView 아이템 선택 리스너 설정
+        binding.bottomNavi.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.fragment_home)
+                    true
+                }
+                R.id.navigation_social -> {
+                    navController.navigate(R.id.fragment_social)
+                    true
+                }
+                R.id.navigation_record -> {
+                    navController.navigate(R.id.fragment_record)
+                    true
+                }
+                R.id.navigation_mypage -> {
+                    navController.navigate(R.id.fragment_mypage)
+                    true
+                }
+                else -> false
             }
         }
-
     }
 
+    fun hideBottomNavigation(state:Boolean){
+        if(state) binding.bottomNavi.visibility = View.GONE else binding.bottomNavi.visibility=View.VISIBLE
+    }
 }
