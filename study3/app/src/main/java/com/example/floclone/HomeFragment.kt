f32aa71de23f7d3133391fe7f74b19e449df3a9e
Santoragi32 <woojj1254577@naver.com>
2025-03-26 12:37:59 +0900

    홈 프래그먼트 추가

---
+++ study3/app/src/main/java/com/example/floclone/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.floclone
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.floclone.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
--- study3/app/src/main/java/com/example/floclone/MainActivity.kt
+++ study3/app/src/main/java/com/example/floclone/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.example.floclone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.floclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fl, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this, SongActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fl, HomeFragment())
+            .commitAllowingStateLoss()
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home_fragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fl, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+//                R.id.look_fragment -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_fl, LookFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+//                }
+//
+//                R.id.search_fragment -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_fl, SearchFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+//                }
+//
+//                R.id.storage_fragment -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_fl, LockerFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+//                }
+            }
+            false
         }
     }
 }
